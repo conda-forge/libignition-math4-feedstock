20b402840b3a3b1216a6c95de5eda161c9961352
Silvio Traversaro <silvio@traversaro.it>
2022-10-20 10:16:28 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ ${target_platform} == "linux-ppc64le" ]; then
+  # Disable tests
+  IGN_TEST_CMD=-DBUILD_TESTING:BOOL=OFF
+  NUM_PARALLEL=-j1
+else
+  IGN_TEST_CMD=-DBUILD_TESTING:BOOL=ON
+  NUM_PARALLEL=
+fi
+
+
 mkdir build
 cd build
 
@@ -10,7 +20,8 @@ cmake ${CMAKE_ARGS} .. \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
       -DUSE_SYSTEM_PATHS_FOR_PYTHON_INSTALLATION:BOOL=ON \
       -DPython3_EXECUTABLE:PATH=$PYTHON \
-      -DPYTHON_EXECUTABLE:PATH=$PYTHON
+      -DPYTHON_EXECUTABLE:PATH=$PYTHON \
+      $IGN_TEST_CMD
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
